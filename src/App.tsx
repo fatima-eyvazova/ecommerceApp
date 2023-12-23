8a558f0912df640c515b681462b508a5c7f14f3b
fatime-eyvazova12 <fatima.eyvazovaa@gmail.com>
2023-12-23 16:19:00 +0400

    persisted state with redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import router from "./router/router";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import basketSliceReducer from "./slices/basketSlice";
 import wishListSliceReducer from "./slices/wishListSlice";
 
-export const store = configureStore({
-  reducer: {
-    basket: basketSliceReducer,
-    wishList: wishListSliceReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  basket: basketSliceReducer,
+  wishList: wishListSliceReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
